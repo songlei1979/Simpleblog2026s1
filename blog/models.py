7aa9bcf89a3d3257cae041cc44446e9268e126f4
Lei <gabriel_sl19798@hotmail.com>
2026-03-16 20:44:27 +1300

    model-view-template

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    header_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    title_tag = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    snippet = models.CharField(max_length=100)
+    likes = models.ManyToManyField(User, related_name='likes')
+
+    def __str__(self):
+        return self.category.name + ' - ' + self.title
+
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from blog.models import Category
+
+
+# Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    return render(request, 'blog/index.html',
+                  {'categories': categories})
