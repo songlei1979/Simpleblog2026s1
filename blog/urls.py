9cd7d9776d7fd6736f5528ee977052dddd2b6659
Lei <gabriel_sl19798@hotmail.com>
2026-03-23 19:54:17 +1300

    template - post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, category_detail, post_detail
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<int:category_id>/',
+         category_detail, name='category_detail'),
+    path('post/<int:post_id>/',
+         post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from blog.models import Category
+from blog.models import Category, Post
 
 
 # Create your views here.
@@ -8,3 +8,13 @@ def index(request):
     categories = Category.objects.all()
     return render(request, 'blog/index.html',
                   {'categories': categories})
+
+def category_detail(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, 'blog/category.html',
+                  {'category': category})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post.html',
+                  {'post': post})
